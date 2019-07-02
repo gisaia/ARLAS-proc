17d7e889a8de45882fb1281f3fb340efd184bd5b
Laurent Thiébaud <laurent.thiebaud@gisaia.com>
2019-07-02 08:16:04 +0200

    HMM without rdd but only dataframe / UDF.
    With real data, processing is now 9 min
    instead of 1 hour

--- src/main/scala/io/arlas/data/transform/HmmProcessor.scala
+++ src/main/scala/io/arlas/data/transform/HmmProcessor.scala
@@ -26,8 +26,14 @@ import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructTy
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.col
 import org.slf4j.LoggerFactory
-import scala.util.{Failure, Success}
 
+import scala.util.{Failure, Success}
+import io.arlas.data.transform.ArlasTransformerColumns._
+import io.arlas.ml.parameter.State
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.expressions.Window
+
+import scala.collection.mutable.ArrayBuffer
 
 class HmmProcessor(dataModel: DataModel,
                    spark                 : SparkSession,
@@ -38,8 +44,11 @@ class HmmProcessor(dataModel: DataModel,
     extends ArlasTransformer(dataModel, Vector(partitionColumn)) {
 
   @transient lazy val logger = LoggerFactory.getLogger(this.getClass)
-  val UNKNOWN_RESULT = "Unknown"
-  val TEMP_COLUMN    = "_tempColumn"
+  val UNKNOWN_RESULT       = "Unknown"
+  val SOURCE_DOUBLE_COLUMN = "_sourceDoubleColumn"
+  val COLLECT_COLUMN       = "_collectColumn"
+  val HMM_PREDICT_COLUMN   = "_hmmPredictColumn"
+  val UNIQUE_ID_COLUMN     = "_uniqueIDColumn"
 
   override def transformSchema(schema: StructType): StructType = {
     checkSchema(schema).add(StructField(resultColumn, StringType, true))
@@ -64,37 +73,53 @@ class HmmProcessor(dataModel: DataModel,
     }
   }
 
+  val hmmPredictUDF = udf((sourceValues: Seq[Double], hmmModelContent: String) => {
+    Hmm.predictStatesSequence(sourceValues, hmmModelContent) match {
+      case Success(statesSequence: Seq[State]) => statesSequence.map(_.state)
+      case Failure(excp) =>
+        logger.error("HMM failed", excp)
+        sourceValues.map(_ => UNKNOWN_RESULT)
+    }
+  })
+
   def interpolateRows(dataset: Dataset[_], hmmModelContent: String) = {
 
-    import spark.implicits._
     val columns = dataset.columns
+    val hmmSchema = StructType(Seq(
+      StructField(UNIQUE_ID_COLUMN, StringType),
+      StructField(resultColumn, StringType)
+    ))
 
-    val interpolatedRows = dataset
-      .withColumn(TEMP_COLUMN, col(sourceColumn).cast(DoubleType))
-      .toDF()
-      .map(row =>
-             (row.getString(row.fieldIndex(partitionColumn)), List(row.getValuesMap(columns :+ TEMP_COLUMN))))
-      .rdd
-      .reduceByKey(_ ++ _)
-      .flatMap {
-                 case (_, timeserie: List[Map[String, Any]]) => {
-                   Hmm.predictStatesSequence(timeserie.map(_.getOrElse(TEMP_COLUMN, 0d)),
-                                             hmmModelContent) match {
-                     case Failure(exception) =>
-                       logger.error("HMM failed", exception)
-                       //do not block the processing, add a default value
-                       timeserie.map(_ + (resultColumn -> UNKNOWN_RESULT))
-                     case Success(statesSequence) =>
-                       timeserie.zip(statesSequence).map {
-                                                           case (ts, state) => ts + (resultColumn -> state.state)
-                                                         }
-                   }
-                 }
-               }
-      .map((entry: Map[String, Any]) =>
-             Row.fromSeq((columns :+ resultColumn).map(entry.getOrElse(_, null)).toSeq))
+    val window = Window.partitionBy(partitionColumn).orderBy(arlasTimestampColumn)
 
-    spark.sqlContext.createDataFrame(interpolatedRows, transformSchema(dataset.schema))
+    val hmmDF = dataset
+      .withColumn(SOURCE_DOUBLE_COLUMN, when(col(sourceColumn).isNull, lit(0.0)).otherwise(col(sourceColumn).cast(DoubleType)))
+      .withColumn(COLLECT_COLUMN, collect_list(SOURCE_DOUBLE_COLUMN).over(window))//use window to ensure timestamp sorting
+      .groupBy(partitionColumn)
+      .agg(last(COLLECT_COLUMN).as(COLLECT_COLUMN))
+    //at this point, if <COLLECT_COLUMN> is an id, rows are like "id1, Seq(0.1, 1.0, 2.0, 3.0)"
+    //the second column is the ordered list of source values to use for prediction
+      .withColumn(HMM_PREDICT_COLUMN, hmmPredictUDF(col(COLLECT_COLUMN), lit(hmmModelContent)))
+    //now rows are like "id1, Seq(PREDICTION1, PREDICTION2, PREDICTION3, PREDICTION4)"
+      .flatMap((r: Row) => {
+        r.getAs[ArrayBuffer[String]](HMM_PREDICT_COLUMN).zipWithIndex.map {
+                                                            case (result: String, index: Int) => {
+                                                              Row.fromSeq(Seq(
+                                                                r.getAs[String](partitionColumn) + "_" + (index + 1),
+                                                                result
+                                                              ))
+                                                            }
+                                                          }
+      //finally we splitted the rows predictions into multiple rows, the first column being a kind of index
+      //like UNIQUE_ID_COLUMN = <partitionColumn>_<row number for its partitionColumn>
+      // e.g. "id1_1, PREDICTION1", "id1_2, PREDICTION2", "id1_3, PREDICTION4", "id1_1, PREDICTION4"
+
+      })(RowEncoder(hmmSchema))
+
+    //we also add the UNIQUE_ID_COLUMN to initial data and join on it
+    dataset
+      .withColumn(UNIQUE_ID_COLUMN, concat(col(partitionColumn), lit("_"), row_number().over(window)))
+      .join(hmmDF, UNIQUE_ID_COLUMN)
+      .drop(UNIQUE_ID_COLUMN)
   }
 }
-
