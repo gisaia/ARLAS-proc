d7b0cf8504ab82a39a16571c51bea8f606c91fd1
Willi <willi.gautier@gisaia.com>
2021-07-30 16:17:54 +0200

    Add licence disclaimer

--- src/main/scala/io/arlas/data/transform/ml/WithTempo.scala
+++ src/main/scala/io/arlas/data/transform/ml/WithTempo.scala
@@ -1,13 +1,35 @@
+/*
+ * Licensed to Gisaïa under one or more contributor
+ * license agreements. See the NOTICE.txt file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Gisaïa licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.arlas.data.transform.ml
 
-import io.arlas.data.model.{DataModel, MLModelLocal}
+import io.arlas.data.model.{MLModelLocal}
 import org.apache.spark.sql.functions.{col, lit, when}
+import org.apache.spark.sql.types.{StructField, StructType, StringType}
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 /**
-  * Identify tempo from duration by using a HMM model
+  * Identify emission tempo of observation from duration by using a HMM model
+  * The emitting object usually record their location regularly in time.
+  * The several modes of emission (ex: 10s, 2min, 30min) can be identify within the data thank to a pre-trained HMM model
   *
-  * @param dataModel     Data model containing names of structuring columns (id, lat, lon, time)
+  * @param idColumn  Name of the column containing object identifier
   * @param durationColumn Name of the column containing the fragment duration (s)
   * @param targetTempoColumn Name of the column to store the detected emission tempo
   * @param spark Spark Session
--- src/main/scala/io/arlas/data/transform/timeseries/WithStateIdOnStateChangeOrUnique.scala
+++ src/main/scala/io/arlas/data/transform/timeseries/WithStateIdOnStateChangeOrUnique.scala
@@ -1,19 +1,46 @@
+/*
+ * Licensed to Gisaïa under one or more contributor
+ * license agreements. See the NOTICE.txt file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Gisaïa licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.arlas.data.transform.timeseries
 
-import io.arlas.data.model.DataModel
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, lag}
 
-class WithStateIdOnStateChangeOrUnique(dataModel: DataModel,
+/**
+  * Create an ID for each sequence of an object id sharing a same state or if the fragment has a "unique state"
+  *
+  * @param idColumn Column containing the id used to create sequences
+  * @param stateColumn Column containing the state to use
+  * @param orderColumn Column containing the field to order sequences (usually timestamp)
+  * @param targetIdColumn Column to store the new created id
+  * @param uniqueState Value of the states that is considered as "unique", the fragment associated has a unique new id
+  */
+class WithStateIdOnStateChangeOrUnique(idColumn: String,
                                        stateColumn: String,
                                        orderColumn: String,
                                        targetIdColumn: String,
                                        uniqueState: String)
     extends WithStateId(
-      dataModel,
+      idColumn,
       orderColumn,
       targetIdColumn, {
-        val window = Window.partitionBy(dataModel.idColumn).orderBy(orderColumn)
+        val window = Window.partitionBy(idColumn).orderBy(orderColumn)
         lag(stateColumn, 1)
           .over(window)
           .notEqual(col(stateColumn))
