4a3368cceb2f89d32201afd0be6c35d9de83be60
Sébastien FALQUIER <sebastien.falquier@gisaia.com>
2021-09-10 14:40:09 +0200

    Fix WithSupportValueTest

--- src/test/scala/io/arlas/data/transform/ml/WithSupportValueTest.scala
+++ src/test/scala/io/arlas/data/transform/ml/WithSupportValueTest.scala
@@ -33,9 +33,9 @@ class WithSupportValueTest extends ArlasTest {
     spark,
     List(
       Seq(10l, 4.0d, 0.1d, "tempo_other", Seq(0.1d)),
-      Seq(10l, 4.0d, 0.2d, "tempo_irregular", Seq(0.4d, 0.4d)),
+      Seq(10l, 4.0d, 0.2d, "tempo_irregular", Seq(0.2d)),
       Seq(100l, 10.0d, 0.3d, "tempo_other", Seq(0.3d)),
-      Seq(100l, 10.0d, 0.4d, "tempo_irregular", Seq.fill(8)(0.1d))
+      Seq(1400l, 10.0d, 0.4d, "tempo_irregular", Seq.fill(10)(0.4d))
     ),
     ListMap(
       arlasTrackDuration -> (LongType, true),
